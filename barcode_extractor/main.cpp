a23ac48b8dc9c215f5c098b93ed4100e4d8559c3
ahfyy <ahfyyyh@outlook.com>
2020-12-09 23:01:25 +0800

    Release commit

---
+++ barcode_extractor/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "BAMReader.h"
+#include "worker.h"
+
+
+
+int main(int argc, char *argv[])
+{
+    if (argc >= 3)
+    {
+        worker *worker1 = new worker(argv[1], argv[2]);
+        if (argc == 3)
+            worker1->extract();
+        else if (argc == 4)
+            worker1->extract_one_target(argv[3]);
+    }
+    else
+        std::cerr << "Usage: BarcodeExtract in_bam out <chr>" << std::endl;
+    return 0;
+}

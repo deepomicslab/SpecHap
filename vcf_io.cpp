383cbaf4f583ffb972c837a0b54d3c1fb60b5cda
caronkey <guangzep@gmail.com>
2022-10-17 15:48:00 +0800

    fix nps not work error

--- vcf_io.cpp
+++ vcf_io.cpp
@@ -271,7 +271,7 @@ void VCFWriter::write_nxt_record(bcf1_t *record, ptr_ResultforSingleVariant resu
     if (resultforSingleVariant->variant_phased()) {
         int *ps_s = nullptr;
         int nps = 0, ps =0;
-//        nps = bcf_get_format_int32(this->header ,record, "PS", &ps_s, &ps);
+        nps = bcf_get_format_int32(this->header ,record, "PS", &ps_s, &ps);
         if (nps <=0 ) {
             ps_s = (int*)malloc(ngt*2*sizeof(int));
             for (int i = 0; i < ngt; ++i) {
@@ -289,7 +289,7 @@ void VCFWriter::write_nxt_record(bcf1_t *record, ptr_ResultforSingleVariant resu
     } else {
         int *ps_s = nullptr;
         int nps = 0, ps =0;
-//        nps = bcf_get_format_int32(this->header ,record, "PS", &ps_s, &ps);
+        nps = bcf_get_format_int32(this->header ,record, "PS", &ps_s, &ps);
         if (nps <= 0) {
             ps_s = (int*)malloc(ngt*2*sizeof(int));
             for (int i = 0; i < ngt; ++i) {
@@ -329,7 +329,9 @@ void VCFWriter::write_nxt_contigs(const char *contig, ChromoPhaser *chromo_phase
         idx2pos[idx] = record->pos + 1;
         ptr_ResultforSingleVariant resultforSingleVariant = chromo_phaser->results_for_variant[idx];
         bcf_translate(this->header, frvcf.header, record);
-
+        if (record->pos == 4565) {
+            auto tmp = 44;
+        }
         if (!is_uninitialized(resultforSingleVariant->block) && resultforSingleVariant->variant_phased() && resultforSingleVariant->get_filter() == filter_type::PASS)
         {
             if (gap_count >= 30)
